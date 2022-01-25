5ab208850971b9c00845c80830b79f8c06f24864
cemil özbay <cemil_ozbay@hotmail.com>
2022-01-26 00:17:49 +0300

    Add project files.

---
+++ ConstructorsInInheritance8523/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsInInheritance8523
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SubClass sub = new SubClass("sub", "base");
+            sub.BaseMethod();
+            sub.SubMethod();
+            
+            Console.ReadLine();
+        }
+    }
+    abstract class BaseClass
+    {
+        private string _baseField;
+
+        //public BaseClass(string baseField)
+        protected BaseClass(string baseField)
+        {
+            _baseField = baseField;
+            Console.WriteLine("Base constructor executed with " + _baseField);
+        }
+        public void BaseMethod()
+        {
+            Console.WriteLine("Base method executed with " + _baseField);
+        }
+    }
+    class SubClass : BaseClass
+    {
+        private string _subField;
+        
+        public SubClass(string subField,string baseField) : base(baseField)
+        {
+            _subField = subField;
+            Console.WriteLine("Sub constructor executed with " + _subField);
+        }
+        public void SubMethod()
+        {
+            Console.WriteLine("Sub method executed with " + _subField);
+        }
+    }
+}
